858d163e0589979789b2223fa0b876c630c1715d
Erica Badu <erica.a.badu@gmail.com>
2015-12-19 20:06:19 -0600

    jquery for photos

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function (){
+	
+	$('.photoA').click(function () {
+
+		$(this).attr('src','images/weddingPhoto2.jpg');
+	});
+
+	$('.photoB').click(function () {
+
+		$(this).attr('src','images/weddingPhoto6.jpg');
+	});
+
+		$('.photoC').click(function () {
+
+		$(this).attr('src','images/weddingPhoto4.jpg');
+	});
+
+
+
+
+});
